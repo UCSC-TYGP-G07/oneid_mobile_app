406a6a7e892a8b63399ffcab28dc3d9574a3c392
SRenuuu <sandulrenuja@gmail.com>
2023-08-09 23:40:08 +0530

    feat: add theme and color files

---
+++ lib/theme/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OneIDColor {
+  static const Color logoColor = Color(0Xff0eac7c);
+  static const Color primaryColor = Color(0xFF27187E);
+  static const Color secondaryColor = Color(0xFF758BFD);
+  static const Color tertiaryColor = Color(0xFFAEB8FE);
+  static const Color accentColor = Color(0xFFFF8600);
+
+  static const Color black = Color(0xFF040405);
+  static const Color lightBlack = Color(0xFF3E404D);
+  static const Color grey = Color(0xFF9D99A7);
+  static const Color lightGrey = Color(0xFFF1F2F6);
+  static const Color darkGrey = Color(0xFF625f6a);
+  static const Color darkBlue = Color(0xFF13165A);
+  static const Color lightBlue = Color(0xFF203387);
+
+  static const Color titleTextColor = Color(0xFF000000);
+  static const Color subTitleTextColor = Color(0xFF797878);
+  static Color bgColor = const Color.fromARGB(255, 255, 255, 255);
+  static Color btnDisabledColor = const Color.fromARGB(255, 194, 202, 212);
+}
